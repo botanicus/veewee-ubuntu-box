b1e465ec6e0b92b479d0577df651e659152ebb71
James C Russell <james@101ideas.cz>
2014-06-15 13:37:33 +0200

    SH doesn't know source, only '.'.

--- definitions/ubuntu-14.04-amd64/extra/convenience.sh
+++ definitions/ubuntu-14.04-amd64/extra/convenience.sh
@@ -11,4 +11,4 @@ chsh -s /bin/zsh vagrant
 # apt-get install -y ack-grep
 curl http://beyondgrep.com/ack-2.04-single-file > /usr/bin/ack && chmod 0755 /usr/bin/ack
 
-npm -g bower uglify-js uglifycss
+npm install -g bower uglify-js uglifycss
