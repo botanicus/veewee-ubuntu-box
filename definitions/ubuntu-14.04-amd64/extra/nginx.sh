ed18732aef8656423dad15e22077029436139ac3
James C Russell <james@101ideas.cz>
2014-06-22 15:53:22 +0200

    Set up MOUNTPOINT.

--- definitions/ubuntu-14.04-amd64/extra/nginx.sh
+++ definitions/ubuntu-14.04-amd64/extra/nginx.sh
@@ -3,7 +3,9 @@ apt-get -y install nginx
 cat > /etc/init/nginx.conf << EOF
 # http://serverfault.com/questions/143461/how-can-i-start-nginx-via-upstart
 
-start on vagrant-mounted
+# Note that environment variables don't expand in start and
+# stop commands: http://upstart.ubuntu.com/cookbook/#restrictions
+start on vagrant-mounted MOUNTPOINT="/vagrant"
 stop on runlevel [!2345]
 
 respawn
